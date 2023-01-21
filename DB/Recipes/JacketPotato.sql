ce90d633b5c93dd8f13aab85208d9bfbd0a27f40
MPZinke <mathew.zinke@gmail.com>
2023-01-21 13:57:54 -0600

    Adds Jacket Potato

---
+++ DB/Recipes/JacketPotato.sql
@@ -0,0 +1,46 @@
+
+/***********************************************************************************************************************
+*                                                                                                                      *
+*   created by: MPZinke                                                                                                *
+*   on 2023.01.21                                                                                                      *
+*                                                                                                                      *
+*   DESCRIPTION: TEMPLATE                                                                                              *
+*   BUGS:                                                                                                              *
+*   FUTURE:                                                                                                            *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+
+-- INSERT INTO "Recipes" ("name", "rating", "serving_size", "time", "instructions") VALUES
+-- ('Jacket Potatoes', 4, 1, INTERVAL '4 HOURS', 
+-- '[
+-- 	"Slice a cross shape about 1/4-inch thick into each potato. This helps them release some steam, makes the interior more fluffy, and also makes them easier to slice into when they''re piping hot.",
+-- 	"Bake at 400°F for ${timer::02:00:00}. The potatoes won''t burn at this temperature and the long bake means the skin will be so crisp that it''s practically cracker-like.",
+-- 	"After the two hours are up, remove the potatoes and carefully cut deeper into the slices you made initially. Then put the potatoes back in the oven for ${timer::00:10:00}. This helps to dry out the flesh further and makes it extra fluffy."
+-- ]');
+
+
+
+-- INSERT INTO "Ingredients" ("brand", "name", "description") VALUES
+-- ('', 'Russet Potatoes', ''),
+-- ('', 'Sea Salt', ''),
+-- ('', 'Black Pepper', ''),
+-- ('', 'Chives', ''),
+-- ('', 'Salted Butter', ''),
+-- ('', 'Sour Cream', '');
+
+
+INSERT INTO "RecipesIngredients" ("Recipes.id", "Ingredients.id", "amount", "quantity", "is_required", "notes")
+SELECT "Recipes"."id", "Ingredients"."id", "Temp"."amount", "Temp"."quantity", "Temp"."is_required", "Temp"."notes"
+FROM
+(
+	VALUES
+	('Jacket Potatoes', 'Russet Potatoes', 1, 'Medium', TRUE, ''),
+	('Jacket Potatoes', 'Sea Salt', 0, 'Course', FALSE, ''),
+	('Jacket Potatoes', 'Black Pepper', 0, 'Ground', FALSE, ''),
+	('Jacket Potatoes', 'Chives', 0, '', FALSE, 'for garnish'),
+	('Jacket Potatoes', 'Salted Butter', 0, '', FALSE, ''),
+	('Jacket Potatoes', 'Sour Cream', 0, '', FALSE, '')
+) AS "Temp"("Recipes.name", "Ingredients.name", "amount", "quantity", "is_required", "notes")
+JOIN "Recipes" ON "Temp"."Recipes.name" = "Recipes"."name"
+JOIN "Ingredients" ON "Temp"."Ingredients.name" = "Ingredients"."name";
--- DB/Schema.sql
+++ DB/Schema.sql
@@ -3,9 +3,10 @@
 CREATE TABLE "Recipes"
 (
 	"id" SERIAL NOT NULL PRIMARY KEY,
+	"is_deleted" BOOLEAN DEFAULT FALSE,
 	"name" VARCHAR(64) NOT NULL UNIQUE,
-	"instructions" TEXT,
-	"rating" INT NOT NULL,
+	"instructions" JSON NOT NULL,
+	"rating" SMALLINT NOT NULL,
 	"serving_size" SMALLINT DEFAULT 1,
 	"time" INTERVAL NOT NULL  -- TODO
 );
@@ -14,20 +15,24 @@ CREATE TABLE "Recipes"
 CREATE TABLE "Ingredients"
 (
 	"id" SERIAL NOT NULL PRIMARY KEY,
+	"is_deleted" BOOLEAN DEFAULT FALSE,
 	"brand" VARCHAR(64) NOT NULL DEFAULT '',
 	"name" VARCHAR(64) NOT NULL UNIQUE,
-	"description" TEXT,
-	UNIQUE ("name", "band")
+	"description" TEXT NOT NULL DEFAULT '',
+	UNIQUE ("name", "brand")
 );
 
 
 CREATE TABLE "RecipesIngredients"
 (
 	"id" SERIAL NOT NULL PRIMARY KEY,
+	"is_deleted" BOOLEAN DEFAULT FALSE,
+	"amount" INT DEFAULT 0,  -- 'TWO' 16oz cans; 0 means there is an unspecified amount
+	"quantity" VARCHAR(64) DEFAULT '',  -- 2 'SIXTEEN OZ' cans
+	"is_required" BOOLEAN DEFAULT TRUE,
+	"notes" TEXT NOT NULL DEFAULT '',
 	"Recipes.id" INT NOT NULL,
 	FOREIGN KEY ("Recipes.id") REFERENCES "Recipes"("id"),
 	"Ingredients.id" INT NOT NULL,
-	FOREIGN KEY ("Ingredients.id") REFERENCES "Ingredients"("id"),
-	"amount" INT NOT NULL DEFAULT 1,  -- TWO 16oz cans
-	"quantity" NUMERIC(6, 2)  -- 2 SIXTEEN OZ cans
+	FOREIGN KEY ("Ingredients.id") REFERENCES "Ingredients"("id")
 );
