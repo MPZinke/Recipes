29ac095de4fcc478cc2f49dedde3d93de623dc4e
MPZinke <mathew.zinke@gmail.com>
2023-02-15 18:08:05 -0600

    Changing decorator to be more versitile

--- Backend/DB/Connection.py
+++ Backend/DB/Connection.py
@@ -14,21 +14,24 @@ def connection_wrapper(function: callable) -> callable:
 	PARAMS:  Takes the function that will be wrapped.
 	RETURNS: The function pointer that wraps the function.
 	"""
-	def wrapper(*args: list, **kwargs: dict) -> Any:
-		"""
-		DETAILS: Creates a connection and passes it to the calling function.
-		RETURNS: Value(s) if values.
-		THROWS:  Whatever exceptions occur during function call.
-		"""
-		DB_user: str = os.getenv("RECIPES_DB_USER")
-		DB_host: str = os.getenv("RECIPES_DB_HOST")
-		DB_password: str = os.getenv("RECIPES_DB_PASSWORD")
+	def decorator(*, ignore: str|list[str]=None):
+		def wrapper(*args: list, **kwargs: dict) -> Any:
+			"""
+			DETAILS: Creates a connection and passes it to the calling function.
+			RETURNS: Value(s) if values.
+			THROWS:  Whatever exceptions occur during function call.
+			"""
+			DB_user: str = os.getenv("RECIPES_DB_USER")
+			DB_host: str = os.getenv("RECIPES_DB_HOST")
+			DB_password: str = os.getenv("RECIPES_DB_PASSWORD")
 
-		connection_string = f"host={DB_host} dbname=Recipes user={DB_user} password={DB_password}"
-		with psycopg2.connect(connection_string) as connection:
-			connection.autocommit = True  # Automatically commit changes to DB
-			with connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
-				return function(cursor, *args, **kwargs)
+			connection_string = f"host={DB_host} dbname=Recipes user={DB_user} password={DB_password}"
+			with psycopg2.connect(connection_string) as connection:
+				connection.autocommit = True  # Automatically commit changes to DB
+				with connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
+					return function(cursor, *args, **kwargs)
 
-	wrapper.__name__ = function.__name__
-	return wrapper
+		wrapper.__name__ = function.__name__
+		return wrapper
+
+	return decorator
